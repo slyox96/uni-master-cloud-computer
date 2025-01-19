9502516fa570c69a1810016a314dd66d4688ec62
Timon <t.froussos@gmail.com>
2025-01-19 15:37:52 +0100

    [+]Modal

---
+++ Milestone_3/Webshop/Frontend/src/components/Modal.tsx
@@ -0,0 +1,45 @@
+import React, { forwardRef, RefObject } from 'react';
+import styles from './Modal.module.scss';
+import PropTypes from 'prop-types';
+import {toggleModal} from '../util/toggleModal'
+
+interface ModalProps {
+    children: React.ReactNode;
+}
+
+export const Modal = forwardRef<HTMLDialogElement, ModalProps>(({ children }, ref) => {
+    return (
+        <dialog
+            ref={ref}
+            onClick={(e) => {
+                if (e.currentTarget === e.target) {
+                    toggleModal(ref as RefObject<HTMLDialogElement>);
+                }
+            }}>
+            <div className={styles.content_dialog}>
+                {children}
+            </div>
+        </dialog>
+    );
+});
+
+
+const ReactNodePropType = (props: Record<string, unknown>, propName: string, componentName: string): Error | null => {
+    const prop = props[propName];
+    if (
+        prop == null ||
+        React.isValidElement(prop) ||
+        typeof prop === 'string' ||
+        typeof prop === 'number' ||
+        typeof prop === 'boolean'
+    ) {
+        return null;
+    }
+    return new Error(`${propName} in ${componentName} is not a valid ReactNode.`);
+};
+
+// Beispiel
+Modal.propTypes = {
+    children: ReactNodePropType,
+};
+Modal.displayName = 'Modal'; // Displays "Modal" in devTools and prevents ESLint warnings
--- Milestone_3/Webshop/Frontend/src/pages/Shop.tsx
+++ Milestone_3/Webshop/Frontend/src/pages/Shop.tsx
@@ -1,13 +1,18 @@
-import React, { useEffect } from 'react'
+import React, { ReactElement, useEffect, useRef, useState } from 'react'
 
 import { useStore } from '../store/store';
 import { Product } from '../types/Product';
 
 import styles from "./Shop.module.scss";
 import ShopingCard from '../components/ShopingCard';
+import { Modal } from '../components/Modal';
+import { toggleModal } from '../util/toggleModal';
 
 export const Shop = () => {
     const { products, isLoading, error, fetchProducts } = useStore();
+    const [modalContent, setModalContent] = useState<ReactElement | null>(null);
+
+    const ModalRef = useRef(null);
 
     useEffect(() => {
         fetchProducts();
@@ -20,9 +25,28 @@ export const Shop = () => {
     if (error) {
         return <div>Error: {error}</div>;
     }
+
+    
+  const openModal = (e: React.MouseEvent) => {
+    e.stopPropagation();
+    setModalContent(
+    <div>
+        Hello World
+    </div>
+    );
+    toggleModal(ModalRef);
+  }
+
     return (
         <>
+            <Modal ref={ModalRef}
+            >
+                <div>
+                    {modalContent}
+                </div>
+            </Modal>
             <div className={styles.searchbar}>
+                <button onClick={(e) => openModal(e)}>Modal</button>
             </div>
             {products.length === 0 ? (
                 <p>No products available</p>
